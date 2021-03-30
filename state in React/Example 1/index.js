260b2af080a7f3fc2137aa3b9e0063cd5fff6e04
simmiagrawal49 <55052741+simmiagrawal49@users.noreply.github.com>
2021-03-30 19:39:29 +0530

    Create index.js

---
+++ state in React/Example 1/index.js
@@ -0,0 +1,17 @@
+//Part 1
+//Implemented a clock thing using props
+import { StrictMode } from "react";
+import ReactDOM from "react-dom";
+
+import App from "./App";
+
+const rootElement = document.getElementById("root");
+ReactDOM.render(
+  <StrictMode> 
+  <App date={new Date()} />
+  </StrictMode>,
+  rootElement
+ 
+);
+
+//Part 2
