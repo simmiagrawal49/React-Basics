bfb001dc29e6c0fed454f824110341496deb7b8d
simmiagrawal49 <55052741+simmiagrawal49@users.noreply.github.com>
2021-03-30 20:34:45 +0530

    Update App.js

--- state in React/Example 1/App.js
+++ state in React/Example 1/App.js
@@ -26,4 +26,31 @@ class App extends React.Component {
 
 export default App;
 
+//when using props with class, this.props is required
 // this.props is required 
+
+
+//part03
+import "./styles.css";
+import React from "react";
+
+class App extends React.Component {
+  //Class components should always call the base constructor with props.
+  
+  //here props/state is a variable, when used like below
+  constructor(props) {
+    super(props);
+    this.state = {date: new Date()};
+  }
+
+  render() {  
+    return (
+      <div>
+        <h1>Hello, world!</h1>
+        <h2>It is {this.state.date.toLocaleTimeString()}.</h2>
+      </div>
+    );
+  }
+}
+
+export default App;
