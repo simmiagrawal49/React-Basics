099071bade06c0304be41231146eec5d5794d297
simmiagrawal49 <55052741+simmiagrawal49@users.noreply.github.com>
2021-03-30 20:35:01 +0530

    Update index.js

--- state in React/Example 1/index.js
+++ state in React/Example 1/index.js
@@ -30,3 +30,16 @@ ReactDOM.render(
 );
 
 //part 3
+import { StrictMode } from "react";
+import ReactDOM from "react-dom";
+
+import App from "./App";
+
+const rootElement = document.getElementById("root");
+ReactDOM.render(
+  <StrictMode> 
+    <App />
+  </StrictMode>,
+  rootElement
+ 
+);
